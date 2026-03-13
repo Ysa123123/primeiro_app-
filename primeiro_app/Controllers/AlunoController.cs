bb00c357d8af6ceeb3f63864f808001a51b0e01b
Ysa123123 <00001096703920sp@aluno.educacao.sp.gov.br>
2026-03-13 15:22:28 -0300

    trabalho sozinha

--- primeiro_app/Controllers/AlunoController.cs
+++ primeiro_app/Controllers/AlunoController.cs
@@ -6,6 +6,8 @@ namespace primeiro_app.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.nota = "aluno nota 10";
+            ViewData["Escola"] = "E.E. Anhanguera";
             return View();
         }
     }
